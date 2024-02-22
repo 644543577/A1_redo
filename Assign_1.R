88ed4fc20c69678687f0f47f080e6a561050d262
644543577 <zhiweixin1@gmail.com>
2024-02-21 22:19:59 -0600

    dev

--- Assign_1.R
+++ Assign_1.R
@@ -3,3 +3,20 @@ git_default_branch()
 # ✔ Setting active project to '/Users/xin/Xin/A1_redo/A1_redo'
 # [1] "main"
 
+library(readr)
+library(tidyverse)
+library(ggplot2)
+olympics_data <- read.csv("Olympics.csv")
+# a
+olympics_data_1 <- olympics_data %>%
+  mutate(total.medals = gold + silver + bronze)
+
+# b
+olympics_data_2 <- olympics_data %>%
+  group_by(country) %>%
+  summarize(total_gold = sum(gold))
+
+# c
+olympics_data_3 <- olympics_data_1 %>%
+  group_by(year) %>%
+  summarize(total_medals = sum(total.medals))
