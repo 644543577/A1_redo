5ed2d3986cf78f8ced9bed9c2772a3f70b26db3f
644543577 <zhiweixin1@gmail.com>
2024-02-21 22:21:35 -0600

    change

--- Assign_1.R
+++ Assign_1.R
@@ -2,4 +2,26 @@ library(usethis)
 git_default_branch()
 # ✔ Setting active project to '/Users/xin/Xin/A1_redo/A1_redo'
 # [1] "main"
+library(readr)
+library(tidyverse)
+library(ggplot2)
+#a:
+olympics_data <- read_csv("Olympics.csv")
+largest_teams_1992 <- olympics_data %>%
+  filter(year == 1992) %>%
+  arrange(desc(athletes)) %>%
+  select(country, athletes)
 
+#b:
+countries <- c("United States", "France", "Germany", "Russia", "China")
+filtered_data <- olympics_data %>%
+  filter(country %in% countries)
+
+ggplot(filtered_data, aes(x = year, y = gold, color = country, group = country)) +
+  geom_line() +
+  geom_point() +
+  theme_minimal() +
+  labs(title = "Gold Medals Over Time",
+       x = "Year",
+       y = "Gold Medals") +
+  theme(legend.title = element_blank())
